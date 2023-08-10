ef095fc007cf972221e77772113f4285f2185c3f
Francisco Andrade <andradef801@gmail.com>
2023-08-09 21:42:33 -0600

    start toolbar work
    started on toolbar got some text and scss going on it. Will work more on it tomorrow.

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -3,7 +3,16 @@ import "./topbar.scss"
 export default function Topbar() {
   return (
     <div className="topbar">
-      
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro">LOGO</a>
+        </div>
+        <h1>Hello</h1>
+        <div className="right">
+          this is the right
+        </div>
+
+      </div>
     </div>
   )
 }
