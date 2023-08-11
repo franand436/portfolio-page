690c9d50fecf388753966c45f8306bac263db014
Francisco Andrade <andradef801@gmail.com>
2023-08-10 21:49:10 -0600

    Started work on intro page
    Got most things finished on intro page. Need to replace stock photo with either logo or a photo of myself. Added text and blinking arrow that pushes visitors to next page. Added asset images.
    
    Need to play with css on image and work on self typing text.

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,25 @@
-import "./intro.scss"
+import "./intro.scss";
 
-export default function Intro () {
+export default function Intro() {
   return (
-    <div className='intro' id="intro">
-      
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/man.png" alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hello, I'm</h2>
+          <h1>Francisco Andrade</h1>
+          <h3>
+            Freelance <span></span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="" />
+        </a>
+      </div>
     </div>
-  )
+  );
 }
