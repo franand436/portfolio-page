132c37873a5110332a7798a5b609e5a3953889b3
Francisco Andrade <andradef801@gmail.com>
2023-08-10 20:29:09 -0600

    Topbar almost done, added menu.
    Added most info to topbar, (will come back to replace text with logo) created hamburger menu animation and added shortcuts in menu for easier site navigation.

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,15 +1,28 @@
 import "./topbar.scss"
+import MailIcon from '@mui/icons-material/Mail';
+import PersonIcon from '@mui/icons-material/Person';
 
-export default function Topbar() {
+export default function Topbar({ menuOpen, setMenuOpen}) {
   return (
-    <div className="topbar">
+    <div className={"topbar " + (menuOpen && "active")}>
       <div className="wrapper">
         <div className="left">
-          <a href="#intro">LOGO</a>
-        </div>
-        <h1>Hello</h1>
+          <a href="#intro" className="logo">LOGO</a>
+          <div className="itemContainer">
+            <PersonIcon className="icon"/>
+            <span>(801)600-9093</span>
+          </div>
+          <div className="itemContainer">
+            <MailIcon className="icon"/>
+            <span>andradef801@gmail.com</span>
+          </div>
+        </div>        
         <div className="right">
-          this is the right
+          <div className="hamburger" onClick={ ()=>setMenuOpen(!menuOpen)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
         </div>
 
       </div>
