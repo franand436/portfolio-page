3d875b80700375260f55c11aeef383157c1c6746
Francisco Andrade <andradef801@gmail.com>
2023-08-11 18:44:35 -0600

    Portfolio page work
    got icons on portfolio page. photoshopped some of the icons so they look like a matching set.

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -5,16 +5,33 @@ export default function portfolio() {
     <div className="portfolio" id="portfolio">
       <h1>Portfolio</h1>
       <ul>
-        <li>Featured</li>
+        <li className="active">Personal Life</li>
         <li>School Programs</li>
-        <li>NAS</li>
-        <li>Media Server</li>
-        <li>This Website</li>
-        <li>Friends and Family IT person</li>
       </ul>
       <div className="container">
         <div className="item">
-          <img src="" alt="" />
+          <img src="assets/unraid.png" alt="" />
+          <h3>Self Hosted NAS</h3>
+        </div>
+        <div className="item">
+          <img src="assets/plex.png" alt="" />
+          <h3>Media Server</h3>
+        </div>
+        <div className="item">
+          <img src="assets/cloudflare.png" alt="" />
+          <h3>Purchased domains and adminstration.</h3>
+        </div>
+        <div className="item">
+          <img src="assets/it.png" alt="" />
+          <h3>Friends and family IT person</h3>
+        </div>
+        <div className="item">
+          <img src="assets/homelab.png" alt="" />
+          <h3>Home Lab</h3>
+        </div>
+        <div className="item">
+          <img src="assets/pythondiscbot.png" alt="" />
+          <h3>Python Discord bot</h3>
         </div>
       </div>
     </div>
